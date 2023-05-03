03b590507318c9fc3301b0be0a9b9e5abc65b9ce
Chuck Carpenter <chuck.carpenter@me.com>
2023-05-03 14:25:20 -0700

    🐛 Remove date render

--- src/components/FormattedDate.jsx
+++ src/components/FormattedDate.jsx
@@ -1,13 +1,13 @@
-const dateFormatter = new Intl.DateTimeFormat('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
+// const dateFormatter = new Intl.DateTimeFormat('en-US', {
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// });
 
 export function FormattedDate({ date, ...props }) {
-  return (
-    <time dateTime={date.toISOString()} {...props}>
-      {date}
-    </time>
+  return (<></>
+    // <time dateTime={date.toISOString()} {...props}>
+    //   {date}
+    // </time>
   );
 }
