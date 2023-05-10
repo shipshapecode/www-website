5d2a890762d4436eac2237346406978c832da701
Robert Wagner <rwwagner90@gmail.com>
2023-05-09 22:01:08 -0400

    Add whiskey loading spinner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,55 @@ import { useEffect, useMemo, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import clsx from 'clsx';
+
 import { fetchEpisodes } from '@/pages/api/episodes/[page]';
 import { useAudioPlayer } from '@/components/AudioProvider';
 import { Container } from '@/components/Container';
 import { FormattedDate } from '@/components/FormattedDate';
 import { dasherize } from '@/utils/dasherize';
-import { truncate } from '@/utils/truncate';
+
+function LoadingIcon({ ...props }) {
+  return (
+    <svg
+      aria-hidden="true"
+      viewBox="0 0 65 65"
+      height="65px"
+      width="65px"
+      {...props}
+    >
+      <path
+        className="fill-current"
+        d="M58.4,8.3c-0.6-2.2-1.7-4.1-8.4-5.5c-4.8-1-17.5-3.5-35.2-0.1C9.6,3.7,7,5.6,6.9,7.6c-0.3,1.8-5.8,32-4,40.2
+	C5.3,58.5,15.3,64,32.7,64c17.6,0,26.9-5.6,29.3-16.6C63.9,39.2,58.9,10.2,58.4,8.3 M32.4,3.1c15.3,0,23.6,3.1,23.6,4.6
+	c0,1.6-8.3,4.6-23.6,4.6S8.9,9.3,8.9,7.8C8.9,6.2,17.2,3.1,32.4,3.1 M60,47.4C57.7,57.4,49.4,62,32.7,62C16.4,62,7.1,57,4.9,47.4
+	c-1.5-6.7,2.3-29.6,3.6-37c1.3,0.9,3.4,1.7,6.4,2.4c4.7,1.1,11,1.7,17.6,1.7s12.9-0.6,17.6-1.7c3-0.7,5.1-1.5,6.4-2.4
+	C57.7,17.7,61.5,40.8,60,47.4"
+      />
+      <path
+        fill="#992709"
+        fill-opacity="0.75"
+        d="M5.4,31.3C4.6,38,4.2,44.4,4.9,47.4C7.1,57,16.4,62,32.7,62c16.7,0,25.1-4.5,27.3-14.6
+	c0.7-3,0.3-9.5-0.5-16.3c-6,2-16.7,5-27.3,5C21.9,36.1,11.5,33.3,5.4,31.3z"
+      >
+        <animate
+          attributeName="d"
+          calcMode="spline"
+          keySplines="0.45 0.03 0.5 0.95; 0.45 0.03 0.5 0.95"
+          values="
+                       M5.4,31.3C4.6,38,4.2,44.4,4.9,47.4C7.1,57,16.4,62,32.7,62c16.7,0,25.1-4.5,27.3-14.6
+	c0.7-3,0.3-9.5-0.5-16.3c-6,2-16.7,5-27.3,5C21.9,36.1,11.5,33.3,5.4,31.3z; 
+                       M5.1,34.6c-0.8,6.8-0.8,9.8-0.2,12.8C7.1,57,16.4,62,32.7,62c16.7,0,25.1-4.5,27.3-14.6
+	c0.7-3,0.6-6.1-0.2-12.9c-6,2-17,4.3-27.7,4.3C21.9,38.8,11.2,36.6,5.1,34.6z;
+                       M5.4,31.3C4.6,38,4.2,44.4,4.9,47.4C7.1,57,16.4,62,32.7,62c16.7,0,25.1-4.5,27.3-14.6
+	c0.7-3,0.3-9.5-0.5-16.3c-6,2-16.7,5-27.3,5C21.9,36.1,11.5,33.3,5.4,31.3z"
+          dur="3s"
+          repeatCount="indefinite"
+        />
+      </path>
+    </svg>
+  );
+}
 
 function PlayPauseIcon({ playing, ...props }) {
   return (
@@ -107,6 +150,7 @@ function EpisodeEntry({ episode }) {
 
 export default function Home({ episodes }) {
   const [recentEpisodes, setRecentEpisodes] = useState(episodes);
+  const [isLoading, setIsLoading] = useState(false);
   // starting from page 2 due to static props fetch of page 1
   const [page, setPage] = useState(2);
 
@@ -117,8 +161,12 @@ export default function Home({ episodes }) {
     'shows and everything in-between.';
 
   async function fetchMoreEpisodes() {
+    setIsLoading(true);
+
     const episodeResponse = await fetch(`/api/episodes/${page}`);
     const moreEpisodes = await episodeResponse.json();
+
+    setIsLoading(false);
 
     setRecentEpisodes([...recentEpisodes, ...moreEpisodes]);
     setPage(page + 1);
@@ -135,7 +183,10 @@ export default function Home({ episodes }) {
       };
     };
     const handleScroll = debounce((ev) => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 500
+      ) {
         fetchMoreEpisodes();
       }
     }, 250);
@@ -166,6 +217,17 @@ export default function Home({ episodes }) {
           {recentEpisodes.map((episode) => (
             <EpisodeEntry key={episode.id} episode={episode} />
           ))}
+        </div>
+
+        <div
+          className={clsx(
+            'flex justify-center text-slate-500',
+            !isLoading && 'hidden'
+          )}
+        >
+          <div className="whiskey-loader relative ">
+            <LoadingIcon />
+          </div>
         </div>
       </div>
     </>
