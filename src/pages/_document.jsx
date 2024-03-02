351f2707007e9d8681b690ce692d809fb43ef2fa
Robert Wagner <rwwagner90@gmail.com>
2024-03-02 10:25:15 -0500

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="bg-white antialiased" lang="en">
-      <Head></Head>
+      <Head>
+        <meta name="apple-itunes-app" content="app-id=1552776603" />
+      </Head>
       <body>
         <Main />
         <NextScript />
